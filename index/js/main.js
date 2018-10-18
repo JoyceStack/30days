c57ae3d00618034bd7677c8be5f5568766a7db11
guodp <guodp9u0@gmail.com>
2018-10-18 22:51:41 +0800

    更新function btnClick

--- index/js/main.js
+++ index/js/main.js
@@ -11,11 +11,15 @@ function submitClick(id) {
 }
 
 function btnClick(id) {
-    var spanDay01 = document.getElementById("day01"); // 找到元素
-    var divInnerprogress01 = document.getElementById("innerprogress01");
-    var nextDays = Number(spanDay01.innerHTML) + 1;
+    var spanDay = document.getElementById("day" + id); // 找到元素
 
-    spanDay01.innerHTML = nextDays;
+    var nextDays = Number(spanDay.innerHTML) + 1;
+
+    var divInnerProgress = document.getElementById("innerProgress" + id);
+    var startDay = document.getElementById("startDay" + id);
+    var endDay = document.getElementById("endDay" + id);
+
+    spanDay.innerHTML = nextDays;
     var percentage = nextDays / 0.3;
 
     var progressStyle = "width: " + percentage + "%";
@@ -23,18 +27,23 @@ function btnClick(id) {
 
     //document.getElementById("tr").className = "styleclass";//styleclass为新的属性值
     //document.getElementById("tr").setAttribute("class","styleclass");
-    divInnerprogress01.setAttribute("style", progressStyle);
+    divInnerProgress.setAttribute("style", progressStyle);
     if (nextDays < 9) {
-        spanDay01.setAttribute("class", "badge badge-danger");
-        divInnerprogress01.setAttribute("class", "progress-bar progress-bar-striped bg-danger");
+        spanDay.setAttribute("class", "badge badge-danger");
+        divInnerProgress.setAttribute("class", "progress-bar progress-bar-striped bg-danger");
+        startDay.setAttribute("class", "badge badge-pill badge-danger");
+        endDay.setAttribute("class", "badge badge-pill badge-danger");
+    } else if (nextDays < 18) {
+        spanDay.setAttribute("class", "badge badge-warning");
+        divInnerProgress.setAttribute("class", "progress-bar progress-bar-striped bg-warning");
+        startDay.setAttribute("class", "badge badge-pill badge-warning");
+        endDay.setAttribute("class", "badge badge-pill badge-warning");
 
-    } else
-    if (nextDays < 18) {
-        spanDay01.setAttribute("class", "badge badge-warning");
-        divInnerprogress01.setAttribute("class", "progress-bar progress-bar-striped bg-warning");
     } else {
-        spanDay01.setAttribute("class", "badge badge-success");
-        divInnerprogress01.setAttribute("class", "progress-bar progress-bar-striped bg-success");
+        spanDay.setAttribute("class", "badge badge-success");
+        divInnerProgress.setAttribute("class", "progress-bar progress-bar-striped bg-success");
+        startDay.setAttribute("class", "badge badge-pill badge-success");
+        endDay.setAttribute("class", "badge badge-pill badge-success");
     }
     if (nextDays >= 30) {
         alert("Congratulations!You have achieved the goal!");
